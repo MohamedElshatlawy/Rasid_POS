c931051091b3028f2e9a100d13fb62c85f4fc064
nermeen50 <nermeensamy50@yahoo.com>
2021-12-20 10:13:28 +0200

    edit5

--- lib/views/commissions/view/commission_view.dart
+++ lib/views/commissions/view/commission_view.dart
@@ -11,6 +11,7 @@ import 'package:rasid_jack/utilities/constants/app_font_styls.dart';
 import 'package:rasid_jack/utilities/localization/localizations.dart';
 import 'package:rasid_jack/utilities/size_config.dart';
 import 'package:rasid_jack/views/commissions/bloc.dart/commission_bloc.dart';
+import 'package:rasid_jack/views/commissions/view/popup_content.dart';
 // import 'package:rasid_jack/common/widgets/custom_drawer.dart';
 import 'package:rxdart/subjects.dart';
 
@@ -82,23 +83,21 @@ class _CommissionViewState extends BaseState<CommissionView> {
       Widget? content}) async {
     return showDialog(
         context: context,
-        builder: (BuildContext context) => SingleChildScrollView(
-              child: AlertDialog(
-                backgroundColor: Colors.transparent,
-                actions: [
-                  AppButton(
-                    width: double.infinity,
-                    style: AppFontStyle.bahijSansArabic(
-                        fontSize: SizeConfig.textFontSize,
-                        fontColor: AppColors.whiteColor),
-                    title: cancelButtonTitle.toString(),
-                    borderColor: AppColors.PINK_COLOR,
-                    backgroundColor: AppColors.PINK_COLOR,
-                    onTap: () => Navigator.pop(context),
-                  )
-                ],
-                content: content!,
-              ),
+        builder: (BuildContext context) => AlertDialog(
+              backgroundColor: Colors.transparent,
+              actions: [
+                AppButton(
+                  width: double.infinity,
+                  style: AppFontStyle.bahijSansArabic(
+                      fontSize: SizeConfig.textFontSize,
+                      fontColor: AppColors.whiteColor),
+                  title: cancelButtonTitle.toString(),
+                  borderColor: AppColors.PINK_COLOR,
+                  backgroundColor: AppColors.PINK_COLOR,
+                  onTap: () => Navigator.pop(context),
+                )
+              ],
+              content: content!,
             ));
   }
 
@@ -123,113 +122,11 @@ class _CommissionViewState extends BaseState<CommissionView> {
             borderColor: AppColors.WHITH_COLOR,
             backgroundColor: AppColors.BUTTON_COLOR,
             onTap: () => showAppDialog(
-              title: '',
-              errorMessage: '',
-              okButtonTitle: '',
-              cancelButtonTitle: AppLocalizations.of(context).back,
-              content: Container(
-                margin: EdgeInsets.symmetric(vertical: SizeConfig.padding * 5),
-                padding: EdgeInsets.symmetric(
-                    horizontal: SizeConfig.padding,
-                    vertical: SizeConfig.padding),
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(30),
-                    color: AppColors.DARK_GRAY_COLOR),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    AppTextFormFieldItem(
-                        title: AppLocalizations.of(context).name,
-                        label: AppText(
-                            label: AppLocalizations.of(context).name,
-                            style: AppFontStyle.bahijSansArabic(
-                                fontSize: SizeConfig.textFontSize,
-                                fontColor: AppColors.whiteColor)),
-                        formFieldItemType: AppFormFieldItemType.USER_NAME,
-                        subject: BehaviorSubject(),
-                        textInputType: TextInputType.name,
-                        labelFontColor: AppColors.whiteColor,
-                        borderColor: AppColors.whiteColor,
-                        focusedBorderColor: AppColors.whiteColor,
-                        iconColor: AppColors.transparentColor,
-                        focusedIconColor: AppColors.whiteColor),
-                    SizedBox(height: SizeConfig.padding),
-                    Container(
-                      decoration: BoxDecoration(
-                          border: Border.all(color: AppColors.WHITH_COLOR),
-                          borderRadius: BorderRadius.circular(10)),
-                      child: StreamBuilder(
-                          stream: bloc.dropdownValuesStream,
-                          builder: (context, AsyncSnapshot snapshot) {
-                            print("object${snapshot.data}");
-                            return AppDropdown(
-                                titleKey: bloc.initalVal.toString(),
-                                style: AppFontStyle.bahijSansArabic(
-                                    fontSize: SizeConfig.textFontSize,
-                                    fontColor: AppColors.whiteColor),
-                                hint: "اسم البنك",
-                                items: snapshot.data ?? [],
-                                onChange: (val) {
-                                  bloc.selectedITem(val['item']);
-                                },
-                                validator: (dynamic valid) {});
-                          }),
-                    ),
-                    SizedBox(height: SizeConfig.padding),
-                    AppTextFormFieldItem(
-                        title: AppLocalizations.of(context).numberofstatement,
-                        formFieldItemType: AppFormFieldItemType.NUMBER,
-                        label: AppText(
-                            label:
-                                AppLocalizations.of(context).numberofstatement,
-                            style: AppFontStyle.bahijSansArabic(
-                                fontSize: SizeConfig.textFontSize,
-                                fontColor: AppColors.whiteColor)),
-                        subject: BehaviorSubject(),
-                        textInputType: TextInputType.name,
-                        labelFontColor: AppColors.whiteColor,
-                        borderColor: AppColors.whiteColor,
-                        focusedBorderColor: AppColors.whiteColor,
-                        iconColor: AppColors.transparentColor,
-                        focusedIconColor: AppColors.whiteColor),
-                    SizedBox(height: SizeConfig.padding * 2),
-                    AppButton(
-                      width: double.infinity,
-                      style: AppFontStyle.bahijSansArabic(
-                          fontSize: SizeConfig.textFontSize,
-                          fontColor: AppColors.whiteColor),
-                      title: AppLocalizations.of(context).sendOrder,
-                      borderColor: AppColors.BUTTON_COLOR,
-                      backgroundColor: AppColors.BUTTON_COLOR,
-                      onTap: () => showAppDialog(
-                          title: '',
-                          errorMessage: '',
-                          okButtonTitle: '',
-                          cancelButtonTitle: AppLocalizations.of(context).back,
-                          content: Container(
-                            margin: EdgeInsets.symmetric(
-                                vertical: SizeConfig.padding * 8),
-                            padding: EdgeInsets.symmetric(
-                                horizontal: SizeConfig.padding,
-                                vertical: SizeConfig.padding * 5),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(30),
-                              color: AppColors.DARK_GRAY_COLOR,
-                            ),
-                            child: AppText(
-                              label: AppLocalizations.of(context)
-                                  .commissionDilalog,
-                              style: AppFontStyle.bahijSansArabic(
-                                  fontSize: SizeConfig.titleFontSize,
-                                  fontColor: AppColors.whiteColor),
-                            ),
-                          )),
-                    ),
-                    SizedBox(height: SizeConfig.padding),
-                  ],
-                ),
-              ),
-            ),
+                title: '',
+                errorMessage: '',
+                okButtonTitle: '',
+                cancelButtonTitle: AppLocalizations.of(context).back,
+                content: PopUpContent(bloc: bloc)),
           ),
           SizedBox(height: SizeConfig.padding),
           AppButton(
